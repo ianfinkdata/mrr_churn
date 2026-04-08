c2e106a1d3cba8187f75c9efb5b3db87db88c0d6
Ian Fink <247514964+ianfinkdata@users.noreply.github.com>
2026-04-08 14:35:57 -0500

    added contract end date to contract table and updated view

--- SQL/contractsummary.sql
+++ SQL/contractsummary.sql
@@ -1,18 +1,22 @@
+use mrr_churn;
+
+ALTER TABLE contract
+ADD COLUMN contract_end_date DATE
+AS (DATE_SUB(DATE_ADD(go_live_deadline, INTERVAL contract_term MONTH), INTERVAL 1 DAY)) VIRTUAL;
 
 
--- CREATE OR REPLACE VIEW contractsummary AS 
+CREATE OR REPLACE VIEW contractsummary AS 
 
 SELECT *, 
 monthly_revenue * COALESCE(contract_term,1) AS total_revenue,
 ROUND((monthly_revenue / go_live_month_days) * go_live_active_days,2)  AS first_month_proration,
 ROUND((monthly_revenue / go_live_month_days),2) as first_month_daily_rate,
 monthly_revenue * COALESCE(contract_term,1) - ROUND((monthly_revenue / go_live_month_days) * go_live_active_days,2) as remaining_contract
-FROM contract
+FROM mrr_churn.contract
 ;
 
-select *,
-DATE_SUB(DATE_ADD(go_live_deadline, INTERVAL contract_term MONTH), INTERVAL 1 DAY) as contract_end_date
-from contractsummary;
+select *
+from mrr_churn.contractsummary;
 
 
 
